ad208e63de51c77e8520288ebda32c5a851cce81
ErnestoSSantos <ernesto.sales@gmail.com>
2023-10-19 19:17:02 -0300

    frist commit ControleFluxo

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+
+public class ParametrosInvalidosException extends Exception {
+    static String contar() throws ParametrosInvalidosException{
+          throw new ParametrosInvalidosException();
+
+    }
+}
